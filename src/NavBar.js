15194faa64089f81e103aee5bc2c78336106d1c9
Angus Bower-Brown <angusbowerbrown@protonmail.com>
2022-09-09 16:17:42 +0100

    added basic navbar

---
+++ src/NavBar.js
@@ -0,0 +1,27 @@
+import React from "react";
+import { NavLink } from "react-router-dom";
+
+function NavBar(){
+    return(
+        <>
+            <h1>NavBar</h1>
+            <NavLink
+            to= "/"
+            exact>
+                Home
+            </NavLink>
+            <NavLink
+            to= "/characters"
+            exact>
+                Characters
+            </NavLink>
+            <NavLink
+            to= "/quotes"
+            exact>
+                Quotes
+            </NavLink>
+        </>
+    )
+}
+
+export default NavBar
