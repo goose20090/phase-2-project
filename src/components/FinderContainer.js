c80efd76515928135f818f344258e22a2175280f
Angus Bower-Brown <angusbowerbrown@protonmail.com>
2022-09-28 14:51:02 +0100

    characterCard component will now appear in the finder container when a successful request to the API is made

--- src/components/FinderContainer.js
+++ src/components/FinderContainer.js
@@ -1,5 +1,6 @@
 import React, {useState} from "react";
 import { StyledFinderForm } from "./component styles/FinderForm.style";
+import { StyledCharacterCard } from "./component styles/CharacterChard.style";
 
 function FinderContainer({className}){
 
@@ -9,6 +10,8 @@ function FinderContainer({className}){
     }
 
     const [formData, setFormData] = useState(initialState)
+
+    const [cards, setCards] = useState([])
 
     function handleChange(e){
         setFormData({
@@ -30,7 +33,7 @@ function FinderContainer({className}){
             })
             .then(res=> res.json())
             .then((res)=>{
-                res.docs.length ? console.log(res.docs[0]) : console.log('no matching character')
+                res.docs.length ? setCards([...cards, res.docs[0]]) : console.log('no matching character')
             })
     }
 
@@ -38,6 +41,7 @@ function FinderContainer({className}){
         <div className={className}>
             <h1>Find Your Party</h1>
             <StyledFinderForm initialState = {initialState} formData = {formData} handleChange= {handleChange} handleSubmit= {handleSubmit}/>
+            {cards.map((card)=> <StyledCharacterCard key = {card._id} card = {card}/>)} 
         </div>
     )
 }
