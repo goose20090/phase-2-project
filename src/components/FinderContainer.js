ae31afe4e840418fc32e36a49473d55950ed40db
Angus Bower-Brown <angusbowerbrown@protonmail.com>
2022-09-28 14:19:01 +0100

    moved state and get request to finderContainer. Also used conditional operator to log different responses based on whether the api request is succesful or not

--- src/components/FinderContainer.js
+++ src/components/FinderContainer.js
@@ -1,14 +1,43 @@
-import React from "react";
+import React, {useState} from "react";
 import { StyledFinderForm } from "./component styles/FinderForm.style";
 
 function FinderContainer({className}){
 
+    const initialState = {
+        name: "",
+        race: ""
+    }
 
+    const [formData, setFormData] = useState(initialState)
+
+    function handleChange(e){
+        setFormData({
+            ...formData,
+            [e.target.name]: `${e.target.value}`
+        })
+        console.log(formData)
+
+    }
+
+    function handleSubmit(e){
+        e.preventDefault()
+        fetch(`https://the-one-api.dev/v2/character?name=${formData.name}`,{
+            method: "GET",
+            headers:{
+                'Accept': 'application/json',
+                'Authorization': 'Bearer DXTHdjzueNO5IyvdtUYC'
+            }
+            })
+            .then(res=> res.json())
+            .then((res)=>{
+                res.docs.length ? console.log(res.docs[0]) : console.log('no matching character')
+            })
+    }
 
     return(
         <div className={className}>
             <h1>Find Your Party</h1>
-            <StyledFinderForm/>
+            <StyledFinderForm initialState = {initialState} formData = {formData} handleChange= {handleChange} handleSubmit= {handleSubmit}/>
         </div>
     )
 }
--- src/components/FinderForm.js
+++ src/components/FinderForm.js
@@ -1,35 +1,6 @@
-import React, { useState } from "react";
+import React from "react";
 
-function FinderForm({className}){
-
-    const initialState = {
-        name: "",
-        race: ""
-    }
-
-    const [formData, setFormData] = useState(initialState)
-
-    function handleChange(e){
-        setFormData({
-            ...formData,
-            [e.target.name]: `${e.target.value}`
-        })
-        console.log(formData)
-
-    }
-
-    function handleSubmit(e){
-        e.preventDefault()
-        fetch(`https://the-one-api.dev/v2/character?name=${formData.name}`,{
-            method: "GET",
-            headers:{
-                'Accept': 'application/json',
-                'Authorization': 'Bearer DXTHdjzueNO5IyvdtUYC'
-            }
-            })
-            .then(res=> res.json())
-            .then(res=>console.log(res))
-    }
+function FinderForm({className, handleChange, handleSubmit, formData}){
 
     return (
             <form className = {className} onSubmit = {handleSubmit}>
