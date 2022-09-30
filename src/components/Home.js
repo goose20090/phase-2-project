525c1d477c4b802ac54dcc5e3b380e3d3f203272
Angus Bower-Brown <angusbowerbrown@protonmail.com>
2022-09-30 17:08:50 +0100

    created progress Bar component using following blog post https://dev.to/ramonak/react-how-to-create-a-custom-progress-bar-component-in-5-minutes-2lcl . Added to party component

--- src/components/Home.js
+++ src/components/Home.js
@@ -4,7 +4,7 @@ function Home(){
     return(
         <>
         <h1>Home</h1>
-        <h2>The forces of Sauron are amassing in the East. The one true ring has revealed itself to Rivendell. You must now build a party of Nine, to take it to be destroyed in the fires of Mount Doom. </h2>
+        <h2>The forces of Sauron are amassing in the East. The one true ring has revealed itself to Rivendell. It is your task to build a party of Nine, to take it to be destroyed in the fires of Mount Doom. </h2>
         </>
     )
 }
--- src/components/Party.js
+++ src/components/Party.js
@@ -1,11 +1,13 @@
 import React from "react";
 import { StyledPartyContainer } from "./component styles/PartyContainer.style";
+import ProgressBar from "./ProgressBar"
 
 function Party({partyCards, setPartyCards, handleCardDelete}){
     return(
         <div>
             <h1>Party Component</h1>
             <StyledPartyContainer partyCards = {partyCards} setPartyCards = {setPartyCards} handleCardDelete = {handleCardDelete}/>
+            <ProgressBar bgcolor = {"red"} completed = {80}/>
         </div>
     )
 }
