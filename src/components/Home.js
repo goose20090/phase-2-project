62513a988f1c41388faf8f8ad27df5b27ee85704
Angus Bower-Brown <angusbowerbrown@protonmail.com>
2022-09-30 16:42:15 +0100

    basic text added to Home component

--- src/components/Home.js
+++ src/components/Home.js
@@ -2,7 +2,10 @@ import React from "react";
 
 function Home(){
     return(
+        <>
         <h1>Home</h1>
+        <h2>The forces of Sauron are amassing in the East. The one true ring has revealed itself to Rivendell. You must now build a party of Nine, to take it to be destroyed in the fires of Mount Doom. </h2>
+        </>
     )
 }
 
